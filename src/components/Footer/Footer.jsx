572a1a86406d65acfb621a7c943ef05e6ee2a602
Adam377 <adamhaigh185@yahoo.co.uk>
2026-09-08 14:51:43 +0100

    Added footer content.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,12 +7,19 @@ const Footer = () => {
                 <div className={styles.footerContent}>
                     <section className={styles.footerColumn}>
                         <h3>INFORMATION</h3>
+                        <p>FAQs</p>
+                        <p>Newsletter</p>
+                        <p>Update Log</p>
+                        <p>Privacy Policy</p>
+                        <p>T&Cs</p>
                     </section>
                     <section className={styles.footerColumn}>
                         <h3>CUSTOMER SERVICE</h3>
+                        <p>Contact Us</p>
                     </section>
                     <section className={styles.footerColumn}>
                         <h3>PRODUCTS</h3>
+                        <p>Scenarios</p>
                     </section>
                 </div>
             </div>
