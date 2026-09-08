ee425df5a4e4cacedecb32abf575791c2e9a4d19
Adam377 <adamhaigh185@yahoo.co.uk>
2026-09-08 11:17:26 +0100

    Started on navbar.

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+// import { Link } from "react-router-dom";
+import React from "react";
+
+import styles from './NavBar.module.css';
+
+const NavBar = () => {
+    return(
+        <>
+            <div className={styles.title}>Adam Haigh</div>
+            
+        </>
+    );
+}
+
+export default NavBar
