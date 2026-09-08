84f8c6fa6ecb0e0b5a44f00e4a30626380dbb0c3
Adam377 <adamhaigh185@yahoo.co.uk>
2026-09-08 15:38:37 +0100

    Added Intro component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import Footer from './components/Footer/Footer.jsx'
 import NavBar from './components/NavBar/NavBar.jsx'
 
+import './App.css';
+import Intro from './components/Intro/Intro.jsx';
+
 function App() {
   
 
   return (
     <>
       <NavBar />
+      <div className="contentContainer">
+        <Intro />
+      </div>
       <Footer />
     </>
   )
