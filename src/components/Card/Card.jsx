fa0eba3d9e7aced14d8c88dec74425be77e1b6d0
Adam377 <adamhaigh185@yahoo.co.uk>
2026-09-11 21:10:03 +0100

    Added min width to Card.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,12 +2,9 @@ import Button from '../Button/Button.jsx';
 
 import styles from './Card.module.css';
 
-// import image from '../../assets/class802TPEGrey.jpg';
-
 const Card = (props) => {
     return (
         <>
-            {/* USE API DATA TO REPLACE HARDCODED DATA */}
             <div className={styles.cardContainer}>
                 <div className={styles.cardImage}>
                     <img src={props.image} />
